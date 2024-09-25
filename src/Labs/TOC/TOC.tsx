d3856705c4a8f7b9edfae561def15493fbce36de
JP-Norton <72099534+JP-Norton@users.noreply.github.com>
2024-09-24 23:22:45 -0400

    Implemented Labs and Kanbas

--- src/Labs/TOC/TOC.tsx
+++ src/Labs/TOC/TOC.tsx
@@ -8,7 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <li><a href="https://github.com/jpnortonwastaken/kanbas-react-web-app" id="wd-github">GitHub Repository</a></li>
+        <li><a href="https://github.com/jpnortonwastaken/kanbas-react-web-app.git" id="wd-github">GitHub Repository</a></li>
       </ul>
     );
   }
